e4a6f12e22e82d2f4b4fb22d23cb1c0e713aa63c
BryanskFront <winsten@mail.ru>
2022-04-26 22:37:30 +0300

    ну ка то так.

--- index.php
+++ index.php
@@ -6,32 +6,31 @@ try {
 } catch(RedisException $e) {
     exit('Connect error');
 }
+$method = $_SERVER['REQUEST_METHOD'];
 $url = parse_url("http://".$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']); #or https
 $dirs = explode('/', $url['path']);
 isset($url['query']) && parse_str($url['query'], $_GET);
-
 $commands_array = array('api');
 $command = isset($dirs[1]) ? $dirs[1] : '' ;
 
 if(in_array($command, $commands_array)){
-    $method = isset($dirs[2]) ? $dirs[2] : '' ;
-    switch($method){
-            case 'redis':
-                $data = [];
+    $action = isset($dirs[2]) ? $dirs[2] : '' ;
+    switch($action){
+        case 'redis':
+            if($method == 'GET'){
+                header('Content-Type: application/json; charset=utf-8');
                 $key = isset($dirs[3]) ? $dirs[3] : '' ;
+                $data = [];
                 if($key != ''){
-                    header('Content-Type: application/json; charset=utf-8');
-                    if($redis->del($key) == '1'){
-                        $data['status'] = true;
-                        $data['code'] = 200;
+                    if($redis->get($key) == false){
+                        $data['code'] = 499;
+                        $data['message']='Key not found';
                     }else{
-                        $data['status'] = false;
-                        $data['code'] = 500;
-                        $data['message'] = "Error message: Wrong KEY";
+                        $redis->del($key);
+                        header('Location: ' . $_SERVER['HTTP_REFERER']);
                     }
                     echo json_encode($data);
                 }else{
-                    header('Content-Type: application/json; charset=utf-8');
                     $allKeys = $redis->keys('*');
                     $i = 0;
                     $data['counter'] = count($allKeys);
@@ -41,18 +40,35 @@ if(in_array($command, $commands_array)){
                         $data[$allKeys[$i]] = $redis->get($allKeys[$i]);
                         $i++;
                     }
-                    echo json_encode($data);
+                echo json_encode($data);
                 }
-            break;
-        }
+            }
+            if($method == 'DELETE'){
+                $key = isset($dirs[3]) ? $dirs[3] : '' ;
+                $data = [];
+                header('Content-Type: application/json; charset=utf-8');
+                if($redis->get($key) == false){
+				    $data['status'] = false;
+                    $data['code'] = 500;
+                    $data['message'] = 'key not found';
+                }else{
+                    $allKeys = $redis->del($key);
+                    $data['status'] = true;
+                    $data['code'] = 200;
+                }
+                echo json_encode($data);
+            }
+        break;
+    }
 }else{
     $allKeys = $redis->keys('*');
     $i = 0;
     echo "<ul>";
     while($i < count($allKeys)){
-        echo "<li id='".$allKeys[$i]."'>".$allKeys[$i]." : ".$redis->get($allKeys[$i])." <a href=/api/del/".$allKeys[$i]."/ class='remove'>delete</a></li>";
+        echo "<li id='".$allKeys[$i]."'>".$allKeys[$i]." : ".$redis->get($allKeys[$i])." <a href=/api/redis/".$allKeys[$i]."/ class='remove'>delete</a></li>";
         $i++;
     }
     echo "</ul>";
 }
+
 ?>
